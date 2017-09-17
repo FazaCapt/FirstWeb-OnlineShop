df950e79ec1a23054691c24fad0eb95cee148848
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-09-17 09:48:16 +0700

    Membuat Component Child

---
+++ src/Child.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Child extends Component {
+    render() { // Satu Method wajib dalam component
+        return <div>Anakan dari faza</div>
+    }
+}
+
+export default Child;
+
+// **** Cara menggunakannya import ke file yang ingin di beri child
--- src/Mine.js
+++ src/Mine.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'; // Import ini syarat dasar untuk membuat component
+import Child from './Child';
 
 class Mine extends Component { // Membuat class component
     render() {
@@ -6,7 +7,13 @@ class Mine extends Component { // Membuat class component
             <div>
                 <div id="mine">Halo Faza Cakep banget</div>
                 <div>Orang paling keren</div>
+                <Child />
+                <br />
+                <Child />
+                <br />
+                <Child />
             </div> // Didalam return hanya ada satu div. Cara mengakalinnya seperti ini
+           
         ) // Bila Component lebih dari satu baris harus menggunakan Dalam kurung
     }
 }
