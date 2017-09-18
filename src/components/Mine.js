d7103482ee71cfd2de388409cbc8dfbfec7d6a46
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-09-18 13:09:07 +0700

    Menangani event changColor and onClick pada constructor

--- src/components/Mine.js
+++ src/components/Mine.js
@@ -8,11 +8,20 @@ import ChildProps from './ChildProps';
 class Mine extends Component { // Membuat class component
     constructor(){
         super()
-        this.state = {texts:[]}
+        this.state = {texts:[], color:''} //Menambahkan state color supaya berubah warna
+        this.changeColor = this.changeColor.bind(this); // Kalo kita membuat function custom maka secara default thisnya null, maka harus dibuat seperti ini.
     }
 
     componentDidMount() {
-        this.setState({texts:['Belajar ReactJS', 'Belajar NodeJS', 'Belajar ExpressJS']});
+        this.setState({texts:['Belajar ReactJS', 'Belajar NodeJS', 'Belajar ExpressJS'], color: 'green'});
+    }
+
+    changeColor(){
+        if(this.state.color === 'green'){
+            this.setState({color: 'blue'})
+        } else {
+            this.setState({color:'green'})
+        }
     }
     render() {
         
@@ -22,8 +31,10 @@ class Mine extends Component { // Membuat class component
             )
         })
 
+        let style = {color: this.state.color} //menambahkan state color
+
         return (
-            <div>
+            <div style = {style} onClick={this.changeColor}>
                 <div id="mine">Welcome in My Website</div>
                 <div>Assalamualaikum</div>
                 <br />
